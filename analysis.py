69eb1159df1c74801daf571aa879ff291437a4ad
Christy Chen <157515727+cyischristy@users.noreply.github.com>
2024-02-11 02:07:11 -0500

    finish task

---
+++ analysis.py
@@ -0,0 +1,29 @@
+"""A Python script to analyze the temperature data."""
+
+import csv
+
+
+def calculate_decadal_average(input_file: str) -> None:
+    """Calculate the decadal average temperature."""
+    with open(input_file, "r", encoding="utf-8") as file:
+        reader = csv.reader(file)
+        clean_data = list(reader)[1:]
+
+    annual_temp = list(zip(*clean_data))[13]
+
+    for i in range(0, len(annual_temp), 10):
+        if i + 10 > len(annual_temp):
+            temps = annual_temp[i:]
+            print(
+                f"Average temperature after {1880+i}: {sum(map(float, temps))/len(temps):.1f}"
+            )
+        else:
+            temps = annual_temp[i : i + 10]
+            print(
+                f"Average temperature for {1880+i}-{1890+i}: "
+                f"{sum(map(float, temps))/len(temps):.1f}"
+            )
+
+
+if __name__ == "__main__":
+    calculate_decadal_average("./annual_temp.csv")
--- munge.py
+++ munge.py
@@ -1,1 +1,43 @@
-# Place code below to do the munging part of this assignment.
+"""A Python script to clean and convert the data to a csv file."""
+
+import re
+
+# Define the pattern for the title and data
+PATTERN_TITLE = r"^([A-Za-z-]+\s+){19}[A-Z+a-z+-]+\n$"
+PATTERN_DATA = r"^(\d{4})\s+(-?\d+\s+){12}(((-?\d+)|\*+)\s+){6}(\d{4})\n$"
+
+
+def celcius_to_fahrenheit(celcius: str) -> str:
+    """Convert degree celcius to fahrenheit"""
+    return f"{float(celcius) / 100 * 9 / 5:.1f}"
+
+
+def munge_data(input_file: str, output_file: str) -> None:
+    """Clean the data and write to a csv file"""
+    clean_data = []
+    with open(input_file, "r", encoding="utf-8") as file:
+        is_title = True
+        # Read the file line by line
+        for line in file:
+            if re.match(re.compile(PATTERN_DATA), line):
+                raw_line = line.strip().split()[:-1]
+                for i, x in enumerate(raw_line):
+                    if i == 0:
+                        pass
+                    elif re.match(re.compile(r"-?\d+"), x):
+                        raw_line[i] = celcius_to_fahrenheit(x)
+                    else:
+                        raw_line[i] = "0.0"
+                clean_data.append(raw_line)
+            if re.match(re.compile(PATTERN_TITLE), line) and is_title:
+                is_title = False
+                clean_data.append(line.strip().split()[:-1])
+
+    # Write the data to a csv file
+    with open(output_file, "w", encoding="utf-8") as file:
+        for line in clean_data:
+            file.write(",".join(line) + "\n")
+
+
+if __name__ == "__main__":
+    munge_data("./data.txt", "./clean_data.csv")
